2a1cc0e357b5179f88f142d6a1b1d057d0ee768a
sosososo24 <suzuki.s.1024@gmail.com>
2020-05-20 22:52:26 +0900

    自動更新
    https://gyazo.com/408b2e9f4394d987fe67900e77492327

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,9 @@
 $(function(){
 
-
   function buildHTML(message){
     if (message.image) {
       var html =
-        `<div class="chat-main__messages__message">
+        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__upper-info">
             <p class="chat-main__messages__message__upper-info__talker">
               ${message.user_name}
@@ -23,7 +22,7 @@ $(function(){
       return html;
     } else {
       var html =
-        `<div class="chat-main__messages__message">
+        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__upper-info">
             <p class="chat-main__messages__message__upper-info__talker">
               ${message.user_name}
@@ -63,7 +62,34 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
-  });
+    });
   })
+    
+    var reloadMessages = function() {
+      var last_message_id = $('.chat-main__messages__message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log(messages)
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.chat-main__messages').append(insertHTML);
+          $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+     }
 
-});
+  });
